5e82f1d09a0cb84e4e6eef3340f174885f350706
Shenelle-Ramkhelawan <31749921+Shenelle-Ramkhelawan@users.noreply.github.com>
2017-10-04 13:28:49 -0400

    Update lab1.c

--- L01_814000456.X/lab1.c
+++ L01_814000456.X/lab1.c
@@ -9,6 +9,7 @@ void main(void)
     int count = 1;          //Initializes the variable 'count' to the value 1
     TRISB = 0;              //Configures PORTB to be an output
     PORTB= 0;
+    
     while(count<16)         //Creates a while loop which will repeat as long as count<16 
     {
         PORTB = count;      //Assigns the value of count to PORTB
